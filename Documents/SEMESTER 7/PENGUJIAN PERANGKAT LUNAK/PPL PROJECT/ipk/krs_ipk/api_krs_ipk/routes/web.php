8fafe8d3daf057be475ce8967f05b9bb6ec70ec0
dheanandaas <142288279+dheanandaas@users.noreply.github.com>
2024-11-14 00:32:38 +0800

    Delete Documents/SEMESTER 7/PENGUJIAN PERANGKAT LUNAK/PPL PROJECT/ipk/krs_ipk directory

--- Documents/SEMESTER 7/PENGUJIAN PERANGKAT LUNAK/PPL PROJECT/ipk/krs_ipk/api_krs_ipk/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MahasiswaController;
-
-Route::get('/krs', [MahasiswaController::class, 'getKRS']);
-Route::get('/', function () {
-    return view('welcome');
-});
